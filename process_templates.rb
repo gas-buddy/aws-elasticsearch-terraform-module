56e55c4e041df973391ce014ed10ac3fb541faf3
Eric Herot <400447+eherot@users.noreply.github.com>
2017-11-09 16:51:25 -0500

    Fix shell globbing to allow process_templates to be run from anywhere

--- process_templates.rb
+++ process_templates.rb
@@ -2,7 +2,7 @@
 
 require 'erb'
 
-Dir.glob('*.erb').each do |tmpl|
+Dir.glob("#{File.dirname(__FILE__)}/*.erb").each do |tmpl|
   result = ERB.new(File.read(tmpl), nil, '-').result
   output_filename = tmpl.sub(/\.erb$/, '')
   File.open(output_filename, 'w') { |f| f.write result }
