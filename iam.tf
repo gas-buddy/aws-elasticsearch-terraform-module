369cfd657706b1a18fa60330e6fe0d89e85c7b1b
Eric Herot <400447+eherot@users.noreply.github.com>
2020-03-26 17:05:56 -0400

    Update syntax for Terraform 0.12

--- iam.tf
+++ iam.tf
@@ -1,13 +1,13 @@
 resource "aws_iam_instance_profile" "this" {
-  name = "${var.iam_instance_profile_name}"
-  role = "${aws_iam_role.this.name}"
+  name = var.iam_instance_profile_name
+  role = aws_iam_role.this.name
 }
 
 resource "aws_iam_role" "this" {
-  name = "${var.iam_instance_profile_name}"
+  name = var.iam_instance_profile_name
   path = "/"
 
-  assume_role_policy = "${data.aws_iam_policy_document.instance-assume-role-policy.json}"
+  assume_role_policy = data.aws_iam_policy_document.instance-assume-role-policy.json
 }
 
 data "aws_iam_policy_document" "instance-assume-role-policy" {
@@ -22,6 +22,7 @@ data "aws_iam_policy_document" "instance-assume-role-policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "this" {
-  role = "${aws_iam_role.this.name}"
+  role       = aws_iam_role.this.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
 }
+
--- security-groups.tf
+++ security-groups.tf
@@ -3,35 +3,35 @@
 #
 
 resource "aws_security_group" "elasticsearch_master" {
-  name = "elasticsearch-${var.cluster_name}-master"
+  name        = "elasticsearch-${var.cluster_name}-master"
   description = "Elasticsearch master group for ${var.cluster_name}"
-  vpc_id = "${var.vpc_id}"
+  vpc_id      = var.vpc_id
 
   tags = {
-    Name = "elasticsearch-${var.cluster_name}-master"
-    cluster = "${var.cluster_name}"
+    Name    = "elasticsearch-${var.cluster_name}-master"
+    cluster = var.cluster_name
   }
 }
 
 # self: tcp/9300-9300
 resource "aws_security_group_rule" "elasticsearch_master_self_9300" {
-  security_group_id = "${aws_security_group.elasticsearch_master.id}"
-  type = "ingress"
-  protocol = "tcp"
-  from_port = 9300
-  to_port = 9300
-  self = true
+  security_group_id = aws_security_group.elasticsearch_master.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 9300
+  to_port           = 9300
+  self              = true
 }
 
 # Rules to allow monitoring
 resource "aws_security_group_rule" "monitoring_elasticsearch_master_9200" {
-  security_group_id = "${aws_security_group.elasticsearch_master.id}"
-  type = "ingress"
-  protocol = "tcp"
-  from_port = 9200
-  to_port = 9200
-  source_security_group_id = "${var.monitor_security_group_id}"
-  description = "Monitoring"
+  security_group_id        = aws_security_group.elasticsearch_master.id
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 9200
+  to_port                  = 9200
+  source_security_group_id = var.monitor_security_group_id
+  description              = "Monitoring"
 }
 
 resource "aws_security_group_rule" "monitoring_elasticsearch_data_9200" {
@@ -47,12 +47,12 @@ resource "aws_security_group_rule" "monitoring_elasticsearch_data_9200" {
 # Egress Rule
 
 resource "aws_security_group_rule" "elasticsearch_master_egress_all" {
-  security_group_id = "${aws_security_group.elasticsearch_master.id}"
+  security_group_id = aws_security_group.elasticsearch_master.id
 
-  type = "egress"
-  from_port = 0
-  to_port = 0
-  protocol = "-1"
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 }
 
@@ -61,67 +61,67 @@ resource "aws_security_group_rule" "elasticsearch_master_egress_all" {
 #
 
 resource "aws_security_group" "elasticsearch_data" {
-  name = "elasticsearch-${var.cluster_name}-data"
+  name        = "elasticsearch-${var.cluster_name}-data"
   description = "Elasticsearch data group for ${var.cluster_name}"
-  vpc_id = "${var.vpc_id}"
+  vpc_id      = var.vpc_id
 
   tags = {
-    Name = "elasticsearch-${var.cluster_name}-data"
-    cluster = "${var.cluster_name}"
+    Name    = "elasticsearch-${var.cluster_name}-data"
+    cluster = var.cluster_name
   }
 }
 
 resource "aws_security_group_rule" "elasticsearch_data_elb_9200" {
-  security_group_id = "${aws_security_group.elasticsearch_data.id}"
+  security_group_id = aws_security_group.elasticsearch_data.id
 
-  source_security_group_id = "${aws_security_group.elb.id}"
-  type = "ingress"
-  protocol = "tcp"
-  from_port = 9200
-  to_port = 9200
+  source_security_group_id = aws_security_group.elb.id
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 9200
+  to_port                  = 9200
 }
 
 # Used Defined Ingress CIDRs
 resource "aws_security_group_rule" "elasticsearch_data_user_cidrs_9200" {
-  count = "${length(var.source_cidrs) > 0 ? 1: 0}"
+  count = length(var.source_cidrs) > 0 ? 1 : 0
 
   security_group_id = aws_security_group.elasticsearch_data.id
-  cidr_blocks = var.source_cidrs
-  type = "ingress"
-  protocol = "tcp"
-  from_port = 9200
-  to_port = 9200
+  cidr_blocks       = var.source_cidrs
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 9200
+  to_port           = 9200
 }
 
 # Used Defined Ingress Groups
 resource "aws_security_group_rule" "elasticsearch_master_elasticsearch_data_9300" {
-  security_group_id = "${aws_security_group.elasticsearch_data.id}"
-  type = "ingress"
-  protocol = "tcp"
-  from_port = 9300
-  to_port = 9300
-  source_security_group_id = "${aws_security_group.elasticsearch_master.id}"
+  security_group_id        = aws_security_group.elasticsearch_data.id
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 9300
+  to_port                  = 9300
+  source_security_group_id = aws_security_group.elasticsearch_master.id
 }
 
 # self: tcp/9300-9300
 resource "aws_security_group_rule" "elasticsearch-data_self_9300" {
-  security_group_id = "${aws_security_group.elasticsearch_data.id}"
-  type = "ingress"
-  protocol = "tcp"
-  from_port = 9300
-  to_port = 9300
-  self = true
+  security_group_id = aws_security_group.elasticsearch_data.id
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 9300
+  to_port           = 9300
+  self              = true
 }
 
 # Egress Rule
 
 resource "aws_security_group_rule" "elasticsearch-data_egress_all" {
-  security_group_id = "${aws_security_group.elasticsearch_data.id}"
+  security_group_id = aws_security_group.elasticsearch_data.id
 
-  type = "egress"
-  from_port = 0
-  to_port = 0
-  protocol = "-1"
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 }
 
